e359f6a15ada28600ef0a85151607a883365594d
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-02-11 13:37:02 +0530

    Added: Displat game board

--- TicTacToe.java
+++ TicTacToe.java
@@ -16,24 +16,22 @@ public class TicTacToe {
         if (choice == 'X') {
             player = 'X';
             computer = 'O';
-            System.out.println("player " + player);
-            System.out.println("computer " + computer);
-        } else if (choice == 'Y') {
+        } else if (choice == 'O') {
             player = 'O';
             computer = 'X';
-            System.out.println("player " + player);
-            System.out.println("computer " + computer);
         }
     }
-    public static void main(String[] args) {
+    public void showboard() {
         System.out.println("Welcome to Tic Tac Toe");
         System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
         System.out.println("---------");
         System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("---------");
         System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
-        TicTacToe obj = new TicTacToe();
-        obj.check();
     }
-
+    public static void main(String[] args) {
+        TicTacToe object = new TicTacToe();
+        object.initialize();
+        object.showboard();
+    }
 }
